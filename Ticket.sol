84dcd9e58baa9a6f09d003784937e1f1756837a3
Bijan Agharafiei <63957104+bijan53c@users.noreply.github.com>
2026-08-14 00:55:23 +0330

    Add files via upload
    Hello world of mine in .sol!

---
+++ Ticket.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Counter {
+  uint256 private count;
+  
+  address public owner;
+
+  uint256 public ticketPrice = 0.01 ether;
+
+  function increment() external onlyOwner{
+    count += 1;
+  }
+
+  function getCount() external view returns (uint256) {
+    return count;
+  }
+
+  function decrement() external payable {
+    require(count > 0, "Sold out");
+    require(msg.value == ticketPrice, "Wrong payment amount");
+    count -= 1;
+  }
+
+
+  
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Not the venue");
+    _;
+  }
+}
+

8b6e4c97ee05b0557bf1b1e6d3d1c4dbf63dbff7
Bijan Agharafiei <63957104+bijan53c@users.noreply.github.com>
2026-09-07 15:15:59 +0330

    Add files via upload

---
+++ SimpleStorage/SimpleStorageMain.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+
+contract SimpleStorage {
+
+    //Structure to save user entry data by contract to blockchain 
+    struct EntryData {
+        string StringData;
+        address Owner;
+    }
+
+    //The list/array to save the entered data into
+    // Letting user to read data by entry index
+    //EntryData [] public DataList;
+
+
+    // Mapping : letting user to gather data by the entered custom ID
+    mapping (uint256 => EntryData) public DataIdToData;
+
+
+    //Add data by array and mapping
+    function AddData(uint256 _ItemID, string memory _StringData) public {
+        //DataList.push (EntryData(_ItemID,_StringData));
+        require (
+            DataIdToData[_ItemID].Owner==msg.sender || 
+            DataIdToData[_ItemID].Owner == address(0),
+            "this ID is assigned to someone else!"
+            );
+        DataIdToData[_ItemID] = EntryData(_StringData, msg.sender);
+    }
+}
